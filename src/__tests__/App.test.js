3d3ec0906118215cd2bd45f0dcea6571d8f58757
Sergey <sergey@kinoafisha.info>
2026-03-02 20:10:31 +0300

    test App

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import App from '../App.vue'
+
+describe('App', () => {
+  it('mounts renders properly', () => {
+    const wrapper = mount(App)
+    expect(wrapper.text()).toContain('Товаров в корзине')
+  })
+})
+
