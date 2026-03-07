6321bfc3d21dcefe4806be13f0d48df0776ace57
Sergey <sergey@kinoafisha.info>
2026-03-07 09:58:58 +0300

    return Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import { provide } from 'vue';
+  import Navigation from './components/prod/Navigation.vue';
   import { useCart } from './composables/useCart';
   const cartStore = useCart()
   provide('cartStore', cartStore)
