e354ab6903f90d52e00efb74c400742ecf294167
Sergey <sergey@kinoafisha.info>
2026-03-07 13:43:45 +0300

    add first e2e test

--- e2e/vue.test.js
+++ e2e/vue.test.js
@@ -1,2 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+test('visits the app root url', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.locator('h1')).toHaveText('Главная страница');
+})
