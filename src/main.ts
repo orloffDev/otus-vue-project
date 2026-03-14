9c4e23b768cc9e85d96e6349514863d4125179e5
Sergey <sergey@kinoafisha.info>
2026-03-14 13:21:33 +0300

    Add typing to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
-import { createApp, provide, h} from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import axios from 'axios'
+import { createApp, provide, h, App as VueApp } from 'vue'
+import { createRouter, createWebHistory, RouteRecordRaw, NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
+import axios, { AxiosInstance } from 'axios'
 import './style.css'
 import App from './App.vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
-import {useUserStore} from "./stores/UserStorage";
-
-import {createPinia} from "pinia";
-import {storeToRefs} from "pinia";
-
-//Натсройки аякса
-axios.defaults.headers.common['X-Request-Ajax'] = 1;
+import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client/core'
+import { useUserStore } from "./stores/UserStorage";
+import { createPinia, Pinia } from "pinia";
+import { storeToRefs } from "pinia";
 
 // Импорт компонентов
 import HomePage from './components/pages/HomePage.vue'
@@ -20,60 +16,108 @@ import ProductPage from './components/pages/ProductPage.vue'
 import AddProduct from './components/pages/AddProduct.vue'
 import AuthPage from './components/pages/AuthPage.vue'
 import NotFoundPage from './components/pages/NotFoundPage.vue'
-import { RouteRecordRaw } from 'vue-router'
 
-// Создаем Apollo Client
-const cache = new InMemoryCache()
-const apolloClient = new ApolloClient({
+// Типизация для мета-полей маршрутов
+declare module 'vue-router' {
+  interface RouteMeta {
+    isRequiresAuth?: boolean
+  }
+}
+
+// Типизация ответа API (если нужно)
+interface ApiResponse<T = any> {
+  data: T
+  status: number
+  statusText: string
+}
+
+// Настройки аякса с типами
+;(axios as AxiosInstance).defaults.headers.common['X-Request-Ajax'] = 1
+
+// Создаем Apollo Client с типами
+const cache: InMemoryCache = new InMemoryCache()
+const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: cache,
   uri: 'https://graphqlzero.almansi.me/api', // Публичное тестовое API
 })
 
-// Определение маршрутов
+// Определение маршрутов с типами
 const routes: RouteRecordRaw[] = [
   { path: '/', component: HomePage },
   { path: '/catalog', component: CatalogPage },
-  { path: '/product/:productId', component: ProductPage, name: 'product'},
-  { path: '/add-product', component: AddProduct,  meta:{ isRequiresAuth: true}},
-  { path: '/auth', component: AuthPage, name: 'auth'},
+  {
+    path: '/product/:productId',
+    component: ProductPage,
+    name: 'product',
+    props: true // передаем productId как пропс
+  },
+  {
+    path: '/add-product',
+    component: AddProduct,
+    meta: { isRequiresAuth: true }
+  },
+  {
+    path: '/auth',
+    component: AuthPage,
+    name: 'auth'
+  },
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: NotFoundPage // создайте этот компонент
+    component: NotFoundPage
   }
 ]
 
-// Создание роутера
+// Создание роутера с типами
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  //Авторизованный пользователь
+// Типизированный навигационный гард
+router.beforeEach((
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+): void => {
+  // Создаем экземпляр хранилища
   const userStore = useUserStore();
-  const {authData} = storeToRefs(userStore);
+  const { authData } = storeToRefs(userStore);
 
+  // Проверяем требует ли маршрут авторизации
   if (to.meta.isRequiresAuth && !authData.value) {
     next({
       name: 'auth',
       query: { redirect: to.fullPath } // Сохраняем куда хотел пользователь
     })
   } else {
-    next() 
+    next()
   }
 })
 
+// Типизация для props компонента
+interface AppProps {
+  apolloClient?: ApolloClient<NormalizedCacheObject>
+}
 
-// Подключение роутера и хранилища(pinia) к приложению
-const app = createApp({
-  setup() {
-    // 2. Предоставляем клиент всему приложению
+// Создание приложения с типами
+const app: VueApp<Element> = createApp({
+  setup(): void {
+    // Предоставляем клиент всему приложению
     provide(DefaultApolloClient, apolloClient)
   },
   render: () => h(App),
 })
-const pinia = createPinia()
+
+// Создание Pinia с типами
+const pinia: Pinia = createPinia()
+
+// Подключение плагинов с типами
 app.use(router)
 app.use(pinia)
+
+// Монтирование приложения
 app.mount('#app')
+
+// Экспорт для использования в других файлах (опционально)
+export { router, pinia, apolloClient }
